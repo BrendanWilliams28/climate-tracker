93f8f8b8cca01445c53930ceb159960dd8f6b5dc
Brendan <brwilliams28@gmail.com>
2020-02-25 15:20:36 -0500

    Initial client styles, packages, and routing

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Climate Change Tracker</h1>
+          <p className='lead'>
+            Track climate change of any US city, with data provided directly
+            from NASA.
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
