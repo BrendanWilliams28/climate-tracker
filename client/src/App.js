786f5e561d6c4c7f81b985bd078e895617f5b38e
Brendan <brwilliams28@gmail.com>
2020-03-24 13:56:38 -0400

    Separated routes to new component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
+import Routes from './components/routing/Routes';
 
 //Redux
 import { Provider } from 'react-redux';
@@ -29,16 +25,11 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Fragment>
-          <Navbar></Navbar>
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-            </Switch>
-          </section>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
