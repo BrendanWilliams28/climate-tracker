7fa152cbeafb425e16e16b852d617300b7882096
Brendan <brwilliams28@gmail.com>
2020-03-25 17:59:19 -0400

    Editted ProfileActions styles

--- client/src/components/user/ProjectActions.js
+++ client/src/components/user/ProjectActions.js
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types';
 const ProjectActions = ({ _id }) => (
   <div className='dash-buttons'>
     <Link to={`/view-project/${_id}`} className='btn btn-primary'>
-      <i className='fas fa-arrow-right text-primary' /> View Project
+      <i className='fas fa-chart-line text-default' /> View Project
     </Link>
     <Link to={`/edit-project/${_id}`} className='btn btn-default'>
-      <i className='fas fa-edit text-primary' /> Edit Project
+      <i className='fas fa-edit text-default' /> Edit Project
     </Link>
     <Link to={`/delete-project/${_id}`} className='btn btn-danger'>
-      <i className='fas fa-trash-alt text-primary' /> View Project
+      <i className='fas fa-trash-alt text-danger' /> Delete Project
     </Link>
   </div>
 );
