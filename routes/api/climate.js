2ae5176aebe8f5b746d7f185621bb60dfa52b404
Brendan <brwilliams28@gmail.com>
2020-04-03 18:27:21 -0400

    Added climate indicator list, and indicator by city to api

---
+++ routes/api/climate.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const config = require('config');
+const axios = require('axios');
+
+// @route  GET api/climate/indicator
+// @desc   Get Indicator List
+// @access Private
+router.get('/indicator', auth, async (req, res) => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: 'https://app.climate.azavea.com/api/indicator',
+      headers: {
+        Authorization: config.get('climateAuth')
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  GET api/climate/climate-data/{city}/{scenario}/indicator/{indicator_name}/
+// @desc   Get Indicator Data by City
+// @access Private
+router.get(
+  '/climate-data/:city/:scenario/indicator/:indicator_name',
+  auth,
+  async (req, res) => {
+    try {
+      const response = await axios({
+        method: 'get',
+        url: `https://app.climate.azavea.com/api/climate-data/${req.params.city}/${req.params.scenario}/indicator/${req.params.indicator_name}/`,
+        headers: {
+          Authorization: config.get('climateAuth')
+        }
+      });
+
+      res.json(response.data);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => res.send('API Running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/projects', require('./routes/api/projects'));
+app.use('/api/climate', require('./routes/api/climate'));
 
 const PORT = process.env.PORT || 5000;
 
