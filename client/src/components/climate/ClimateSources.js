5ad8dd4f9860b588233157aa2357cea8dc509585
Brendan <brwilliams28@gmail.com>
2020-04-19 14:21:25 -0400

    Changed effect hook to only get data sources once

--- client/src/components/climate/ClimateSources.js
+++ client/src/components/climate/ClimateSources.js
@@ -10,7 +10,7 @@ const ClimateSources = ({
   auth
 }) => {
   useEffect(() => {
-    getClimateSources();
+    if (Object.keys(sources).length === 0) getClimateSources();
   }, [getClimateSources]);
 
   return (
