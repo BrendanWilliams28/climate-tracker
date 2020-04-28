fe05ed3b61b3289b3545326ce38f32ccbfba69d8
Brendan <brwilliams28@gmail.com>
2020-04-27 20:25:48 -0400

    Updated navbar to material-ui

--- client/src/components/layout/Navbar_test.js
+++ client/src/components/layout/Navbar_test.js
@@ -1,22 +1,32 @@
 import React, { Fragment } from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
-import logo from '../../img/logo.png';
 import { makeStyles } from '@material-ui/core/styles';
+import logo from '../../img/logo.png';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import DashboardIcon from '@material-ui/icons/Dashboard';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles(theme => ({
   grow: {
     flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
   },
   title: {
     display: 'none',
@@ -24,8 +34,42 @@ const useStyles = makeStyles(theme => ({
       display: 'block'
     }
   },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   inputRoot: {
     color: 'inherit'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch'
+    }
   },
   sectionDesktop: {
     display: 'none',
@@ -62,12 +106,6 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     handleMobileMenuClose();
   };
 
-  const handleMenuCloseAndLogout = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-    logout();
-  };
-
   const handleMobileMenuOpen = event => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
@@ -83,10 +121,8 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-      <MenuItem onClick={handleMenuClose} component={Link} to='/edit-profile'>
-        Edit Profile
-      </MenuItem>
-      <MenuItem onClick={handleMenuCloseAndLogout}>Logout</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
     </Menu>
   );
 
@@ -101,15 +137,25 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      <MenuItem component={Link} to='/dashboard'>
-        <IconButton aria-label='Show My Cities' color='inherit'>
-          <DashboardIcon />
+      <MenuItem>
+        <IconButton aria-label='show 4 new mails' color='inherit'>
+          <Badge badgeContent={4} color='secondary'>
+            <MailIcon />
+          </Badge>
         </IconButton>
-        <p>Dashboard</p>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label='show 11 new notifications' color='inherit'>
+          <Badge badgeContent={11} color='secondary'>
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
       </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
-          aria-label='User Profile'
+          aria-label='account of current user'
           aria-controls='primary-search-account-menu'
           aria-haspopup='true'
           color='inherit'
@@ -125,30 +171,58 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <div className={classes.grow}>
       <AppBar position='static'>
         <Toolbar>
-          <img src={logo} alt='Climate Hub' />
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='open drawer'
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant='h6' noWrap>
+            Climate Hub
+          </Typography>
 
           {!loading && isAuthenticated ? (
             <Fragment>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder='Search…'
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput
+                  }}
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </div>
               <div className={classes.grow} />
               <div className={classes.sectionDesktop}>
-                <MenuItem component={Link} to='/dashboard'>
-                  <IconButton aria-label='Show My Cities' color='inherit'>
-                    <DashboardIcon />
-                  </IconButton>
-                  <p>Dashboard</p>
-                </MenuItem>
-
-                <MenuItem onClick={handleProfileMenuOpen}>
-                  <IconButton
-                    aria-label='User Profile'
-                    aria-controls={menuId}
-                    aria-haspopup='true'
-                    color='inherit'
-                  >
-                    <AccountCircle />
-                  </IconButton>
-                  <p>Profile</p>
-                </MenuItem>
+                <IconButton aria-label='show 4 new mails' color='inherit'>
+                  <Badge badgeContent={4} color='secondary'>
+                    <MailIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton
+                  aria-label='show 17 new notifications'
+                  color='inherit'
+                >
+                  <Badge badgeContent={17} color='secondary'>
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton>
+                <IconButton
+                  edge='end'
+                  aria-label='account of current user'
+                  aria-controls={menuId}
+                  aria-haspopup='true'
+                  onClick={handleProfileMenuOpen}
+                  color='inherit'
+                >
+                  <AccountCircle />
+                </IconButton>
               </div>
               <div className={classes.sectionMobile}>
                 <IconButton
